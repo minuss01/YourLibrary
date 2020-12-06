0ebc7797fed10ad9b7a302aafee3a8998f3a9a71
Mariusz Fila <mariusz.stasiu.fila@gmail.com>
2020-12-06 22:56:28 +0100

    Added swagger to API project

--- YourLibrary.API/Startup.cs
+++ YourLibrary.API/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace YourLibrary.API
 {
@@ -26,6 +20,16 @@ namespace YourLibrary.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = "",
+                        Description = "",
+                        Version = "v1"
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +49,14 @@ namespace YourLibrary.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(opt => 
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "YourLibrary");
+                opt.DocExpansion(DocExpansion.None);
             });
         }
     }
